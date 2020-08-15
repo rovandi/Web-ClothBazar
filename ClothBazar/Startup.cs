361c6ef85d142a1c82a2bebd4cab9ab382f0ecf5
rovandi iskandar <rovandii46@gmail.com>
2020-08-16 04:10:23 +0700

    Add project files.

---
+++ ClothBazar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBazar.Startup))]
+namespace ClothBazar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
